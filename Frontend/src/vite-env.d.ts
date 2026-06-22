fb89cfe4307a823f53684897c7b31111bf99b942
Melkias Teshoma <melkiasapo@gmail.com>
2026-06-22 11:32:06 +0300

    logo

--- Frontend/src/vite-env.d.ts
+++ Frontend/src/vite-env.d.ts
@@ -1,5 +1,15 @@
 /// <reference types="vite/client" />
 
+declare module '*.svg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.png' {
+  const src: string;
+  export default src;
+}
+
 declare global {
   interface Window {
     TradingView?: {
